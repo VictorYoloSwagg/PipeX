8de4918665ab5b3e100fa1026e5e818f5c9c209f
Victor Pramann <vpramann@fu-r1-p5.s19.be>
2024-07-29 18:35:29 +0200

    commit

--- pipeX.h
+++ pipeX.h
@@ -6,10 +6,14 @@
 /*   By: vpramann <vpramann@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 18:02:42 by vpramann          #+#    #+#             */
-/*   Updated: 2024/07/08 18:05:01 by vpramann         ###   ########.fr       */
+/*   Updated: 2024/07/29 16:29:03 by vpramann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
+
+# include <unistd.h>
+# include <string.h>
+
 #endif
--- pipex.c
+++ pipex.c
@@ -1,7 +1,55 @@
-int main(int argc, char **argv)
-{
-    char **args;
-    int i;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vpramann <vpramann@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/29 16:11:23 by vpramann          #+#    #+#             */
+/*   Updated: 2024/07/29 18:20:14 by vpramann         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    return 0;
+#include "pipex.h"
+
+int	main(int argc, char **argv, char **env)
+{
+	char	**args;
+	int		*fds;
+
+	if (argc != 5)
+		return 0;
+	fds[0] = open(argc[1], O_RDONLY);
+	fds[1] = open(argc[5], O_WRONLY);
+	pipe(fds);
+	fork();
+	if ()
+	{
+		
+	}
+	else
+	{
+		
+	}
+	/*
+	open file 1
+	trouver commande1
+	open file 2
+	trouver commande2
+	creer pipe
+	fork?
+	child
+		dup2
+		fermer file1
+		femer pipe0
+		exec commande1
+		
+	parent
+		dup2
+		fermer pipe1
+		exec commande 2
+		
+	fermer 
+	*/
+	return 0;
 }

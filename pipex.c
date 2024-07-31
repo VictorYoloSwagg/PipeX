57de34a5dc771413cfa0bc106e34139315637cfd
Victor Pramann <vpramann@shi-r4-p5.s19.be>
2024-07-31 22:03:56 +0200

    commit

--- pipex.c
+++ pipex.c
@@ -6,18 +6,26 @@
 /*   By: vpramann <vpramann@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 16:11:23 by vpramann          #+#    #+#             */
-/*   Updated: 2024/07/30 16:15:38 by vpramann         ###   ########.fr       */
+/*   Updated: 2024/07/31 22:03:47 by vpramann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(int argc, char **argv, char **env)
+char	*getpath(char **envp)
+{
+	int	i;
+	int	j;
+	
+	
+}
+
+int	main(int argc, char **argv, char **envp)
 {
 	char	**args;
 	int		fds[2];
 	pid_t	pid0;
-	pid_t	pid1;
+	/*pid_t	pid1;*/
 	
 
 	if (argc != 5)
@@ -26,15 +34,21 @@ int	main(int argc, char **argv, char **env)
 	fds[1] = open(argc[5], O_WRONLY);
 	if (pipe(fds) < 0)
 		exit (1);
+	if((pid0 = fork()) < 0)
+	{
+		exit (1);
+	}
 	if((pid0 = fork()) > 0);
 	{
 		close(pid0);
 		dup2(fds[0], STDIN_FILENO);
+		execve(/*pathname*/, argv[2], envp);
 	}
 	else
 	{
-		close(pid1);
+		close(pid0);
 		dup2(fds[1], STDOUT_FILENO);
+		execve(/*pathname*/, argv[3], envp);
 	}
 	/*
 	open file 1
